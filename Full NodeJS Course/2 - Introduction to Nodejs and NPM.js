b40070a681fa2bc79f3b4e4835014f64523765db
Noman Ali <aliyounas084@gmail.com>
2024-03-23 23:52:36 +0500

    First Module start

---
+++ Full NodeJS Course/2 - Introduction to Nodejs and NPM.js
@@ -0,0 +1,20 @@
+// NodeJS is JS Runtime Built On Google Chrome's V8 JS Engine
+
+// We can use NodeJS to run JS on the server
+
+// When to use NodeJS
+    // -> Single Threaded based on event driven non-blocking I/O model
+    // -> Perfect for building fast and scalable data-intensive apps
+    // -> Real-time chat apps, live streaming apps, etc.
+    // -> Server Side web applications
+
+// NodeJS is not good for CPU intensive apps
+    // -> Image manipulation, video encoding, etc.
+    // -> Because it's single threaded
+    // -> Applications that require heavy computation should be built using other languages like Python, Java, etc.
+
+
+// Companies like NETFLIX, UBER, PAYPAL, etc. use NodeJS
+// Another reason to use NodeJS is that it's built on JS, so you can use the same language on the client and server side
+// NodeJS is open source and has a large community
+// Huge libray of modules available through NPM (Node Package Manager)
