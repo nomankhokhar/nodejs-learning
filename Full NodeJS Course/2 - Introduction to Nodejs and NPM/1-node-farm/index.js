fd41381d611b79c0746b64bbf7be3f57cfafbbfb
Noman Ali <aliyounas084@gmail.com>
2024-03-26 04:49:48 +0500

    Template Card Show Using JSON Object Data

--- Full NodeJS Course/2 - Introduction to Nodejs and NPM/1-node-farm/index.js
+++ Full NodeJS Course/2 - Introduction to Nodejs and NPM/1-node-farm/index.js
@@ -43,19 +43,45 @@ const http = require('http');
 
 
 // first this will return string data then we convert it into JSON using JSON.parse()
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const productData = JSON.parse(data);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);
+
+const replaceTemplate = (tempCard, el)=> {
+    let output = tempCard.replace(/{%PRODUCTNAME%}/g, el.productName);
+    output = output.replace(/{%IMAGE%}/g, el.image);
+    output = output.replace(/{%PRICE%}/g, el.price);
+    output = output.replace(/{%FROM%}/g, el.from);
+    output = output.replace(/{%NUTRIENTS%}/g, el.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, el.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, el.description);
+    output = output.replace(/{%ID%}/g, el.id);
+
+    if(!el.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+
+}
 
 const server = http.createServer((req, res)=>{
     const pathName = req.url;
-    if (pathName === '/overview'){
-        res.end("This is the Overview Page");
+    if (pathName === '/' ||  pathName === '/overview'){
+        
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.end(output);
+
     }else if(pathName === '/product'){
+        
         res.end("This is the Product Page");
+    
     }else if (pathName === '/api'){
         
-
         res.writeHead(200, {
             'Content-Type' : 'application/json'
         });
