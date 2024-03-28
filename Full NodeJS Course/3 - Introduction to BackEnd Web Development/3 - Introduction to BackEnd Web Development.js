82591ce6c99eef380a62680aa9329f8386c0b7d0
Noman Ali <aliyounas084@gmail.com>
2024-03-28 21:39:38 +0500

    3rd Module Done and Learn Basic Static, Dynamic and API

---
+++ Full NodeJS Course/3 - Introduction to BackEnd Web Development/3 - Introduction to BackEnd Web Development.js
@@ -0,0 +1,26 @@
+// request Response model or Client server architecture
+// How request and response work in web development
+// What is the difference between frontend and backend in Web Development
+// Most of the Theoretical Concept
+
+
+// Static vs Dynamic vs API Website
+
+
+// Static Website serves files to the browser without any server side processing
+
+
+// Dynamic Website serves files to the browser after server side processing that contain databases 
+
+// database -> Get data -> Build website -> Send to browser mean client
+//                              |
+//                              v
+//                           Template
+
+
+// databases -> Get Data --------> JSON then send to the Client or Browser
+
+// Server Side Rendered mean files send from the server to the browser then render the page
+// Client Side Rendered mean data send to the browser then render the page
+
+// API making mean we can use these API in Browser, Native Mobile Apps, Native Desktop Apps, IOT Devices, etc
