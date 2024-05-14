1b1782fd68f1be9a4371e2f315aa0c1eca6625b9
Noman Ali <aliyounas084@gmail.com>
2024-05-15 00:18:20 +0500

    fact : server static files using Express.statuc

--- Full NodeJS Course/6 - Express Lets Start Building the Natours API/NatureAPI/index.js
+++ Full NodeJS Course/6 - Express Lets Start Building the Natours API/NatureAPI/index.js
@@ -8,6 +8,8 @@ const userRouter = require('./routes/userRoutes');
 
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
+
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware')
@@ -18,7 +20,7 @@ app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     req.noman = 'Noman Ali'
     next()
-})
+}) 
 
 
 // One way to create routes
